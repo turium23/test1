e80f45146761b2d590e2bf78931c6e2a90c477cc
turium23 <143045629+turium23@users.noreply.github.com>
2023-08-29 15:54:53 +1000

    Add files via upload

---
+++ t5_vicuna_3b.py
@@ -0,0 +1,24 @@
+t5_vicuna_3b = {
+    "category": "<10B",
+    "display_name": "T5 Vicuna",
+    "thumb": "https://i.ibb.co/4W7n78b/chansung-vector-logo-of-collective-intelligence-of-cute-llamas-3ef46884-72e6-44da-b88a-e831e5fee747.png",
+    "thumb_mid": "https://i.ibb.co/zZzSkQT/t5-vicuna-mid.png",
+    "thumb_tiny": "https://i.ibb.co/xhpY41G/t5-vicuna-tiny.png",
+    "parameters": "3",
+    "vram(full)": "7830",
+    "vram(8bit)": "5224",
+    "vram(4bit)": "4324",
+    "vram(gptq)": "N/A",
+    "hub(base)": "lmsys/fastchat-t5-3b-v1.0",
+    "hub(ckpt)": "N/A",
+    "hub(gptq)": "N/A",
+    "hub(gptq_base)": "N/A",
+    "default_gen_config": "configs/response_configs/t5_vicuna.yaml",
+    "desc": "This model is based on [Flan-T5-XL](https://huggingface.co/google/flan-t5-xl) from google, and it is fine-tuned on [ShareGPT](https://sharegpt.com/) dataset curated by [LMSYS](https://lmsys.org/) in [Vicuna style](https://github.com/lm-sys/FastChat)",
+    "example1": [
+        [
+            "Compose an engaging travel blog post about a recent trip to Hawaii, highlighting cultural experiences and must-see attractions.",
+            "Sure, here's a travel blog post about a recent trip to Hawaii:\nHawaii has been on my mind for the past few months. I've finally had the opportunity to visit the island paradise and have come to terms with how much I love it. Here are a few highlights from my stay:\n\n1. Culture of Hawaii: From ancient Polynesian artifacts and traditional Hawaiian cuisine to colorful luaus and breathtaking sunsets, Hawaii is rich in cultural experiences. In addition to its vibrant culture, Hawaii is also known for its diverse landscapes and stunning natural beauty.\n2. Must-See Attractions: One of my favorite attractions in Hawaii was the historic Honolulu city, which is located in the heart of Honolulu. The city is famous for its beautiful beaches and lush foliage, as well as its beautiful architecture and historical landmarks.\n3. Cultural Experiences: When in Hawaii, you can't go wrong visiting local cultural events such as luaus and luau parties. These events are full of fun and excitement, and they provide a glimpse into the local way of life.\n4. Nature Activities: Hawaii has so much to offer, from hiking the majestic Maui mountains to kayaking down the Pacific Ocean. There are plenty of opportunities to get out and explore nature in this tropical paradise.\n5. Weather Conditions: Hawaii often sees rainy days and warm temperatures, making it a great time to visit the islands. However, with careful planning, you can still enjoy the beach and explore some of the best sights in Hawaii.\n6. Food and Drink:If you want to experience something a little more adventurous, consider spending a day surfing or playing golf at one of the many popular beaches on the islands.\n7. Overall, Hawaii is a truly unique place that has so much to offer visitors. With its diverse cultures, stunning natural beauty, and delicious food and drinks, there's no shortage of reasons to visit again and again."
+        ]
+    ]
+}
